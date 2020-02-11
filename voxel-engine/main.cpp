f543f7526a25fdc1a1683e3bcf076d2a7ce5cc02
LiamHz <liam.hinzman@gmail.com>
2020-02-11 13:08:16 -0500

    Initial Commit

---
+++ voxel-engine/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  voxel-engine
+//
+//  Created by Liam Hinzman on 2020-02-11.
+//  Copyright © 2020 Liam Hinzman. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
