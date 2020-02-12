47a57e4ed7b84db553b83fae0af7749d4f3c3447
LiamHz <liam.hinzman@gmail.com>
2020-02-12 15:34:50 -0500

    Created pizza scene

--- voxel-engine/main.cpp
+++ voxel-engine/main.cpp
@@ -33,11 +33,14 @@ int init();
 int processInput(GLFWwindow *window);
 inline int get_self_product(vector<int> x);
 int set_vertex_attribs(long vboOffset, int nAttrib);
+void create_pepper_r(int x, int z, int t, chunkVecs cv);
+void create_pepper_l(int x, int z, int t, chunkVecs cv);
+void create_pepperoni(int x, int z, int t, chunkVecs cv);
 void mouse_callback(GLFWwindow *window, double xpos, double ypos);
 void create_chunk(vector<int> p, vector<int> d, int t, chunkVecs &cv);
 int createTexture(unsigned int textures[8], std::string texPath, int i);
 void add_rgb_to_palette(std::vector<glm::vec3> &palette, int r, int g, int b);
-int create_scene(Shader shader, unsigned int textures[8], unsigned int VAO, unsigned int VBO, vector<vector<int>> &dim, vector<int> &tex);
+int create_scene(Shader shader, unsigned int textures[8], unsigned int VAO, unsigned int VBO, vector<vector<int>> &dim, vector<int> &tex, std::vector<glm::vec3> &palette);
 
 // Camera
 Camera camera(glm::vec3(4.0f, 4.0f, 10.0f));
@@ -71,20 +74,15 @@ int main() {
     glGenTextures(8, textures);
     vector<vector<int>> dim;
     vector<int> tex;
+    std::vector<glm::vec3> palette;
     
-    if (create_scene(shader, textures, VAO, VBO, dim, tex) != 0)
+    if (create_scene(shader, textures, VAO, VBO, dim, tex, palette) != 0)
         return -1;
     
     // Set origin world coordinates
     shader.use();
     glm::mat4 model = glm::mat4(1.0f);
     shader.setMat4("model", model);
-    
-    // Color palette
-    std::vector<glm::vec3> v_colors;
-    
-    add_rgb_to_palette(v_colors, 253, 253, 150);
-    add_rgb_to_palette(v_colors, 153, 153, 150);
     
     while (!glfwWindowShouldClose(window)) {
         vertSize = 0;
@@ -114,8 +112,8 @@ int main() {
         // Render each object in VBO with its associated texture
         for (int i = 0; i < dim.size(); i++) {
             vertSize = get_self_product(dim[i]) * nVerts;
-            // shader.setInt("u_texture", tex[i]);
-            shader.setVec3("u_color", v_colors[tex[i]]);
+//             shader.setInt("u_texture", tex[i]);
+            shader.setVec3("u_color", palette[tex[i]]);
             glDrawArrays(GL_TRIANGLES, vertOffset, vertSize);
             vertOffset += vertSize;
         }
@@ -148,8 +146,35 @@ void create_chunk(vector<int> p, vector<int> d, int t, chunkVecs &cv){
     cv.tex.push_back(t);
 }
 
-int create_scene(Shader shader, unsigned int textures[8], unsigned int VAO, unsigned int VBO, vector<vector<int>> &dim, vector<int> &tex) {
-    int nCubes = 144;
+void create_pepperoni(int x, int z, int t, chunkVecs cv) {
+    create_chunk({  x,  1,  z}, { 3,  1,  1}, t, cv);
+    create_chunk({x-1,  1,z-1}, { 5,  1,  1}, t, cv);
+    create_chunk({x-2,  1,z-4}, { 7,  1,  3}, t, cv);
+    create_chunk({x-1,  1,z-5}, { 5,  1,  1}, t, cv);
+    create_chunk({  x,  1,z-6}, { 3,  1,  1}, t, cv);
+}
+
+void create_pepper_r(int x, int z, int t, chunkVecs cv) {
+    create_chunk({  x,  1,  z}, { 2,  1,  1}, t, cv);
+    create_chunk({x-1,  1,z-1}, { 3,  1,  1}, t, cv);
+    create_chunk({x-1,  1,z-2}, { 2,  1,  1}, t, cv);
+}
+
+void create_pepper_l(int x, int z, int t, chunkVecs cv) {
+    create_chunk({  x,  1,  z}, { 2,  1,  1}, t, cv);
+    create_chunk({  x,  1,z-1}, { 3,  1,  1}, t, cv);
+    create_chunk({x+1,  1,z-2}, { 2,  1,  1}, t, cv);
+}
+
+void create_olive(int x, int z, int t, chunkVecs cv) {
+    create_chunk({  x,  1,  z}, { 1,  1,  1}, t, cv);
+    create_chunk({x-1,  1,z-1}, { 1,  1,  1}, t, cv);
+    create_chunk({x+1,  1,z-1}, { 1,  1,  1}, t, cv);
+    create_chunk({  x,  1,z-2}, { 1,  1,  1}, t, cv);
+}
+
+int create_scene(Shader shader, unsigned int textures[8], unsigned int VAO, unsigned int VBO, vector<vector<int>> &dim, vector<int> &tex, std::vector<glm::vec3> &palette) {
+    int nCubes = 1024;
     long vboOffset = 0;
     vector<vector<int>> pos;
     chunkVecs cv(pos, dim, tex);
@@ -161,13 +186,69 @@ int create_scene(Shader shader, unsigned int textures[8], unsigned int VAO, unsi
     
     glBindVertexArray(VAO);
     
-//    createTexture(textures, "container.jpg", 0);
-//    createTexture(textures, "stone_iron.png", 1);
+    createTexture(textures, "container.jpg", 0);
+    createTexture(textures, "stone_iron.png", 1);
     
-    create_chunk({ 0,  0,  0}, { 8,  1,  8}, 1, cv);
-    create_chunk({ 0,  0,  0}, { 1,  5,  1}, 0, cv);
-    create_chunk({ 7,  0,  0}, { 1,  5,  1}, 0, cv);
-    create_chunk({ 6,  0,  0}, { 1,  7,  1}, 1, cv);
+    add_rgb_to_palette(palette, 253, 253, 150);
+    add_rgb_to_palette(palette, 255, 105,  96);
+    add_rgb_to_palette(palette, 119, 221, 119);
+    add_rgb_to_palette(palette,   0,   0,   0);
+    add_rgb_to_palette(palette, 131, 105,  83);
+    
+    // Cheese
+    std::vector<int> cheeseDepth{2, 3, 2, 3, 2, 3, 2, 2, 3, 2, 3};
+    
+    int zIndex = 0;
+    int depth;
+    
+    // Cheese grows with pattern
+    for (int i = 0; i < cheeseDepth.size(); i++) {
+        depth = cheeseDepth[i];
+        zIndex += depth;
+        create_chunk({-i, 0, -zIndex}, {i*2 + 1, 1, depth}, 0, cv);
+    }
+    
+    // At the back, cheese shrinks in width
+    int s = (int)cheeseDepth.size();
+    create_chunk({-s + 2, 0, -zIndex - 1}, {(s-2)*2 + 1, 1, 1}, 0, cv);
+    create_chunk({-s + 5, 0, -zIndex - 2}, {(s-5)*2 + 1, 1, 1}, 0, cv);
+    create_chunk({-s + 9, 0, -zIndex - 3}, {(s-9)*2 + 1, 1, 1}, 0, cv);
+    
+    // Pepperonis
+    create_pepperoni(-1, -7, 1, cv);
+    create_pepperoni( 2,-16, 1, cv);
+    create_pepperoni(-6,-21, 1, cv);
+    
+    // Peppers
+    create_pepper_r( 3, -12, 2, cv);
+    create_pepper_l(-6, -18, 2, cv);
+    create_pepper_l( 4, -24, 2, cv);
+    
+    // Olive
+    create_olive(-3, -14, 3, cv);
+    create_olive( 0, -27, 3, cv);
+    
+    // Crust
+    // Left
+    create_chunk({-11, 0, -28}, {2, 2, 1}, 4, cv);
+    create_chunk({-11, 0, -29}, {5, 2, 1}, 4, cv);
+    create_chunk({-11, 0, -30}, {9, 2, 1}, 4, cv);
+    // Middle
+    create_chunk({ -9, 0, -31}, {19, 2, 1},4, cv);
+    create_chunk({ -6, 0, -32}, {13, 2, 1},4, cv);
+    create_chunk({ -2, 0, -33}, {5, 2, 1}, 4, cv);
+    // Right
+    create_chunk({ 10, 0, -28}, {2, 2, 1}, 4, cv);
+    create_chunk({  7, 0, -29}, {5, 2, 1}, 4, cv);
+    create_chunk({  3, 0, -30}, {9, 2, 1}, 4, cv);
+    // Top
+    create_chunk({-10, 2, -29}, {1, 1, 1}, 4, cv);
+    create_chunk({ -9, 2, -30}, {3, 1, 1}, 4, cv);
+    create_chunk({ -6, 2, -31}, {4, 1, 1}, 4, cv);
+    create_chunk({ -2, 2, -32}, {5, 1, 1}, 4, cv);
+    create_chunk({  3, 2, -31}, {4, 1, 1}, 4, cv);
+    create_chunk({  7, 2, -30}, {3, 1, 1}, 4, cv);
+    create_chunk({  8, 2, -29}, {1, 1, 1}, 4, cv);
     
     for (int i = 0; i < dim.size(); i++)
         draw_chunk(pos[i], dim[i], ci);
@@ -247,7 +328,7 @@ int init() {
     glViewport(0, 0, screenWidth, screenHeight);
     
     // Enable wireframe mode
-    // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+//    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
     // Enable z-buffer
     glEnable(GL_DEPTH_TEST);
